eb047ec3d65f3ad3162d323317418078c6f2841f
Tommy Vo <nghia.vo.0509@gmail.com>
2016-10-28 11:57:00 -0400

    pushed request.py which uses REST API and stream.py which uses STREAM API

---
+++ stream.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+# https://www.dataquest.io/blog/streaming-data-python/
+import time
+from getpass import getpass
+from textwrap import TextWrapper
+
+import tweepy
+
+
+class StreamWatcherListener(tweepy.StreamListener):
+
+    status_wrapper = TextWrapper(width=60, initial_indent='    ', subsequent_indent='    ')
+
+    def on_status(self, status):
+        try:
+            print status
+            print self.status_wrapper.fill(status.text)
+            print '\n %s  %s  via %s\n' % (status.author.screen_name, status.created_at, status.source)
+        except:
+            # Catch any unicode errors while printing to console
+            # and just ignore them to avoid breaking application.
+            pass
+
+    def on_error(self, status_code):
+        print 'An error has occured! Status code = %s' % status_code
+        return True  # keep stream alive
+
+    def on_timeout(self):
+        print 'Snoozing Zzzzzz'
+
+
+def main():
+    access_token = "308609794-UnsFrbl4fcBQsOzbG5sqliFMKowhOlzRmLHVeBdp"
+    access_token_secret = "BwjfaD1QgiF0wEzMdyBDsLLEnYXeXLpLqgVcru4oU9QLB"
+    consumer_key = "Tzui94xupQIdChpAD7shh6DVo"
+    consumer_secret = "uMRrB7tH7YHgANboYa3wB1U0HHGm2g51j9Aj7QG9XVZnLgRlv9"
+
+    auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = tweepy.Stream(auth, StreamWatcherListener(), timeout=None)
+
+    # Prompt for mode of streaming
+    valid_modes = ['sample', 'filter']
+    while True:
+        mode = raw_input('Mode? [sample/filter] ')
+        if mode in valid_modes:
+            break
+        print 'Invalid mode! Try again.'
+
+    if mode == 'sample':
+        stream.sample()
+
+    elif mode == 'filter':
+        follow_list = raw_input('Users to follow (comma separated): ').strip()
+        track_list = raw_input('Keywords to track (comma seperated): ').strip()
+        if follow_list:
+            follow_list = [u for u in follow_list.split(',')]
+            userid_list = []
+            username_list = []
+            
+            for user in follow_list:
+                if user.isdigit():
+                    userid_list.append(user)
+                else:
+                    username_list.append(user)
+            
+            for username in username_list:
+                user = tweepy.API().get_user(username)
+                userid_list.append(user.id)
+            
+            follow_list = userid_list
+        else:
+            follow_list = None
+        if track_list:
+            track_list = [k for k in track_list.split(',')]
+        else:
+            track_list = None
+        print follow_list
+        print track_list
+        stream.filter(follow_list, track_list, languages=['en'])
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print '\nGoodbye!'
