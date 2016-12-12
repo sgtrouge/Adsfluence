38df7b78a71aea920a0c6a5511b7204e8cfff05f
ErWarner <ericcwarner@gmail.com>
2016-12-12 14:59:10 -0500

    Add files via upload

---
+++ retweet_friends.py
@@ -0,0 +1,74 @@
+import oauth2
+import time
+import json
+from pprint import pprint
+from django.utils.encoding import smart_str
+import pickle
+import csv
+#w = open("retweet_friends.csv", "w")
+
+# TODO: Integrate with a list of users to get follower graph.
+def oauth_req(url, key, secret, http_method="GET", post_body='', http_headers=None):
+	consumer = oauth2.Consumer(key='rtSAieJdzAhsALalsFdEU7Xup', secret=	'5q3f3MzZyV9wZVc9IeqZxFiO7l9S6jzFD1Ta4kkVuu9aWTtzf4')
+	token = oauth2.Token(key=key, secret=secret)
+	client = oauth2.Client(consumer, token)
+	cursor = -1
+	t = 0
+	while cursor != 0:
+		try:
+			cursor_url = url + '&cursor='+ str(cursor)
+			resp, content = client.request( cursor_url, method=http_method, body=post_body, headers=http_headers)
+			tmp = json.loads(content)
+			cursor = tmp['next_cursor']
+			print (len(tmp['ids']))
+		except:
+			waitTime = 15
+			print ("Rate limit exceeded, waiting for " + str(waitTime) + " minutes",t)
+			t +=1
+			if t>1:
+				content = []
+				break
+			time.sleep(60*waitTime)
+
+	return content
+
+
+json_data = open("result.txt",'r')
+
+Total = ''
+Vector = []
+
+for line in json_data:
+	for letter in line:
+		Total+=letter
+		if letter == "}":
+			Vector.append(Total)
+			Total = ''
+
+Users = []
+i=0
+
+for v in Vector:
+	try:
+		Test = json.loads(v)
+		print Test
+		i+=1
+		if i==10:
+			break
+		if Test["retweeted_author_id"] not in Users:
+			Users.append(Test["retweeted_author_id"])
+	except:
+		continue
+
+    
+# find who a specific user follows. Change the screen name below
+Friends = {}
+"""
+for user in Users:
+	home_timeline = oauth_req( 'https://api.twitter.com/1.1/friends/ids.json?user_id=' +str(user), '80996230-1AQmU1rFemflCOW3B2I6w8OTxGDv0Qq97zI6fZVpZ', 'WY7T0QjGE5q98KDDsIiPnpjQuTXCmGkzanzYSUYMt8Ng6' )
+	if home_timeline !=[]:
+		ids = json.loads(home_timeline)["ids"]
+		Friends = {}
+		Friends[user] = [ids]
+		json.dump(Friends, w)
+"""
